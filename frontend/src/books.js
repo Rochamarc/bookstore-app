2e45ff64f65530bba6e90b8a631f599dbf49592f
karkraz <rochadcmarcos@gmail.com>
2024-09-14 18:17:17 -0300

    add frontend react

---
+++ frontend/src/books.js
@@ -0,0 +1,35 @@
+const books = [
+    {
+      title: "O Senhor dos Anéis",
+      author: "J.R.R. Tolkien",
+      genre: "Fantasia",
+      literature: "Inglesa",
+      publishedAt: "1954",
+      wasRead: true,
+      timesRead: 3,
+      toBuy: false
+    },
+    {
+      title: "Dom Casmurro",
+      author: "Machado de Assis",
+      genre: "Romance",
+      literature: "Brasileira",
+      publishedAt: "1899",
+      wasRead: true,
+      timesRead: 1,
+      toBuy: false
+    },
+    {
+      title: "1984",
+      author: "George Orwell",
+      genre: "Distopia",
+      literature: "Inglesa",
+      publishedAt: "1949",
+      wasRead: false,
+      timesRead: 0,
+      toBuy: true
+    }
+  ];
+  
+  export default books;
+  
