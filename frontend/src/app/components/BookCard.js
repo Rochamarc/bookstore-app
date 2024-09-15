1009454241d3a6b651150b9a12892d3d4ecd8ef7
karkraz <rochadcmarcos@gmail.com>
2024-09-15 15:35:43 -0300

    scope(frontend): using components on get books and authors

--- frontend/src/app/components/BookCard.js
+++ frontend/src/app/components/BookCard.js
@@ -1,24 +1,29 @@
-// Componente BookCard que recebe as props do livro e renderiza as informações
+import Link from 'next/link';
+
 export default function BookCard({ book }) {
-    return (
-      <div style={{
-        backgroundColor: 'white',
-        border: '1px solid #ddd',
-        margin: '10px',
-        padding: '20px',
-        width: '300px',
-        borderRadius: '8px',
-        boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)'
-      }}>
-        <h2>{book.title}</h2>
-        <p><strong>Autor:</strong> {book.author_name}</p>
-        <p><strong>Nacionalidade do Autor:</strong> {book.author_nationality}</p>
-        <p><strong>Foi lido:</strong> {book.was_read}</p>
-        <p><strong>Vezes lido:</strong> {book.times_read}</p>
-        <p><strong>Comprar:</strong> {book.to_buy}</p>
-        <p><strong>Criado em:</strong> {new Date(book.created_at).toLocaleDateString()}</p>
-        <p><strong>Última atualização:</strong> {new Date(book.updated_at).toLocaleDateString()}</p>
-      </div>
-    );
-  }
-  
+  return (
+    <div style={{
+      backgroundColor: 'white',
+      border: '1px solid #ddd',
+      margin: '10px',
+      padding: '20px',
+      width: '300px',
+      borderRadius: '8px',
+      boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)'
+    }}>
+      <h2>{book.title}</h2>
+      <p><strong>Author:</strong> {book.author_name}</p>
+      <p><strong>Author Nationality:</strong> {book.author_nationality}</p>
+      <p><strong>Was Read:</strong> {book.was_read}</p>
+      <p><strong>Times Read:</strong> {book.times_read}</p>
+      <p><strong>To Buy:</strong> {book.to_buy}</p>
+      <Link href={`/books/${book.url.split('/').slice(-2, -1)}`}>
+        View Book Details
+      </Link>
+      <br />
+      <Link href={`/authors/${book.author.split('/').slice(-2, -1)}`}>
+        View Author Details
+      </Link>
+    </div>
+  );
+}
